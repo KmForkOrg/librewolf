497db40f5088e97ed6215b6ac14cd8a956eba5c0
any1here <any1here@duck.com>
2026-09-23 20:36:03 +0200

    Use correct path

--- scripts/librewolf-patches.py
+++ scripts/librewolf-patches.py
@@ -107,7 +107,9 @@ def librewolf_patches():
     exec("cp -r ../themes/browser .")
 
     # copy our patch icons
-    exec('cp -v ../assets/icons/* toolkit/themes/shared/icons/')
+    exec('cp -v ../assets/icons/canvas.svg toolkit/themes/shared/icons/')
+    exec('cp -v ../assets/icons/webgl.svg browser/themes/shared/icons/webgl.svg')
+    exec('cp -v ../assets/icons/webgl-blocked.svg browser/themes/shared/icons/webgl-blocked.svg')
 
     # copy the right search-config.json-v2 file and search-config-icons file
     exec('cp -v ../assets/search-config-v2.json services/settings/dumps/main/search-config-v2.json')
